89281702887ce14b54c824f4b0e3ffc05cf0f590
Elisa Dankers <ejcmdankers@gmail.com>
2022-12-01 20:46:36 +0100

    Day 1 complete

--- day1.py
+++ day1.py
@@ -7,8 +7,11 @@ for elf in data:
     items = elf.split('\n')
     items = [int(n) for n in items]
     sum_calories = sum(items)
+    if sum_calories > max_calories:
+        max_calories = sum_calories
     if sum_calories > min(top_three):
         min_calories = top_three.index(min(top_three))
         top_three[min_calories] = sum_calories
 
+print(max_calories)
 print(sum(top_three))
