ba543947ec6de4e992a6022ed423f9194a5edf4d
Elisa <e.j.c.m.dankers@student.utwente.nl>
2022-12-01 09:01:27 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+data = open('input1.txt','r').read().split('\n\n')
+
+max_calories = 0
+item = []
+top_three = [0, 0,0]
+for elf in data:
+    items = elf.split('\n')
+    items = [int(n) for n in items]
+    sum_calories = sum(items)
+    if sum_calories > min(top_three):
+        min_calories = top_three.index(min(top_three))
+        top_three[min_calories] = sum_calories
+
+print(sum(top_three))
